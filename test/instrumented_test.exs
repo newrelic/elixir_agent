ea87a7298875324b9edff96867640239f478eb97
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2018-09-12 19:16:44 -0300

    Add `request/5` to HTTPoison instrumenter

--- test/instrumented_test.exs
+++ test/instrumented_test.exs
@@ -10,4 +10,9 @@ defmodule InstrumentedTest do
     {:ok, response} = NewRelic.Instrumented.HTTPoison.get("http://www.google.com")
     assert response.status_code == 200
   end
+
+  test "HTTPoison request" do
+    {:ok, response} = NewRelic.Instrumented.HTTPoison.request(:get, "http://www.google.com")
+    assert response.status_code == 200
+  end
 end
