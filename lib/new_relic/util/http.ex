d0f16201780d11da4b6cd41fea42b9efad637c81
Vince Foley <vincefoley@gmail.com>
2018-12-06 21:34:34 -0800

    Leverage ssl_verify_fun

--- lib/new_relic/util/http.ex
+++ lib/new_relic/util/http.ex
@@ -2,14 +2,14 @@ defmodule NewRelic.Util.HTTP do
   @moduledoc false
 
   def post(url, body, headers) when is_binary(body) do
-    url = ~c(#{url})
-    headers = for({k, v} <- headers, do: {~c(#{k}), ~c(#{v})})
+    %{host: host} = URI.parse(url)
+    headers = Enum.map(headers, fn {k, v} -> {'#{k}', '#{v}'} end)
 
     {:ok, {{_, status_code, _}, _headers, body}} =
       :httpc.request(
         :post,
-        {url, headers, 'application/json', body},
-        NewRelic.Util.SSL.ssl_options(),
+        {'#{url}', headers, 'application/json', body},
+        ssl_options(host),
         []
       )
 
@@ -18,4 +18,14 @@ defmodule NewRelic.Util.HTTP do
 
   def post(url, body, headers),
     do: post(url, Jason.encode!(body), headers)
+
+  def ssl_options(host) do
+    [
+      ssl: [
+        verify: :verify_peer,
+        cacertfile: Application.app_dir(:new_relic_agent, "priv/cacert.pem"),
+        verify_fun: {&:ssl_verify_hostname.verify_fun/3, [{:check_hostname, '#{host}'}]}
+      ]
+    ]
+  end
 end
