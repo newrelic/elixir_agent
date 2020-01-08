a764ce77465430e5915a60d748c7f93c30597852
Vince Foley <vince@newrelic.com>
2020-01-07 16:07:59 -0800

    use shared helpers

--- examples/apps/ecto_example/test/ecto_example_test.exs
+++ examples/apps/ecto_example/test/ecto_example_test.exs
@@ -12,20 +12,20 @@ defmodule EctoExampleTest do
   end
 
   test "basic HTTP request flow" do
-    restart_harvest_cycle(Collector.Metric.HarvestCycle)
+    TestHelper.restart_harvest_cycle(Collector.Metric.HarvestCycle)
 
     {:ok, %{body: body}} = request()
     assert body =~ "world"
 
-    metrics = gather_harvest(Collector.Metric.Harvester)
+    metrics = TestHelper.gather_harvest(Collector.Metric.Harvester)
 
-    assert find_metric(
+    assert TestHelper.find_metric(
              metrics,
              "Datastore/statement/Postgres/counts/insert",
              1
            )
 
-    assert find_metric(
+    assert TestHelper.find_metric(
              metrics,
              "Datastore/statement/MySQL/counts/insert",
              1
@@ -39,21 +39,5 @@ defmodule EctoExampleTest do
       :httpc.request('http://localhost:#{http_port}/hello')
 
     {:ok, %{body: to_string(body)}}
-  end
-
-  defp gather_harvest(harvester) do
-    Process.sleep(300)
-    harvester.gather_harvest
-  end
-
-  defp restart_harvest_cycle(harvest_cycle) do
-    GenServer.call(harvest_cycle, :restart)
-  end
-
-  defp find_metric(metrics, name, call_count) do
-    Enum.find(metrics, fn
-      [%{name: ^name}, [^call_count, _, _, _, _, _]] -> true
-      _ -> false
-    end)
   end
 end
