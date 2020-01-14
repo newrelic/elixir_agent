b4b32395536cb0b5240dce70bcd0e17b3607bf9b
Vince Foley <vince@newrelic.com>
2020-01-13 16:38:46 -0800

    tracestate parsing

--- lib/new_relic/w3c_trace_context/trace_state.ex
+++ lib/new_relic/w3c_trace_context/trace_state.ex
@@ -1,2 +1,114 @@
 defmodule NewRelic.W3CTraceContext.TraceState do
+  defstruct [:members]
+
+  defmodule State do
+    defstruct [
+      :version,
+      :parent_type,
+      :account_id,
+      :app_id,
+      :span_id,
+      :transaction_id,
+      :sampled,
+      :priority,
+      :timestamp
+    ]
+  end
+
+  def encode(%__MODULE__{members: members}) do
+    members
+    |> Enum.map(&encode/1)
+    |> Enum.join(",")
+  end
+
+  def encode(%{vendor: :new_relic, state: state, trusted_account_key: trusted_account_key}) do
+    value =
+      [
+        state.version,
+        state.parent_type |> encode_type(),
+        state.account_id,
+        state.app_id,
+        state.span_id,
+        state.transaction_id,
+        state.sampled |> encode_sampled(),
+        state.priority,
+        state.timestamp
+      ]
+      |> Enum.join("-")
+
+    "#{trusted_account_key}@nr=#{value}"
+  end
+
+  def encode(%{vendor: vendor, value: value}) do
+    "#{vendor}=#{value}"
+  end
+
+  def decode(header) when is_binary(header) do
+    members =
+      header
+      |> String.split(",")
+      |> Enum.map(&String.split(&1, "="))
+      |> Enum.map(&decode/1)
+
+    %__MODULE__{members: members}
+  end
+
+  def decode([vendor, value]) do
+    decode(vendor_type(vendor), vendor, value)
+  end
+
+  def decode(:new_relic, vendor, value) do
+    [
+      version,
+      parent_type,
+      account_id,
+      app_id,
+      span_id,
+      transaction_id,
+      sampled,
+      priority,
+      timestamp
+    ] = String.split(value, "-")
+
+    [trusted_account_key, _] = String.split(vendor, "@")
+
+    %{
+      vendor: :new_relic,
+      value: value,
+      trusted_account_key: trusted_account_key,
+      state: %__MODULE__.State{
+        version: version |> String.to_integer(),
+        parent_type: parent_type |> decode_type(),
+        account_id: account_id,
+        app_id: app_id,
+        span_id: span_id,
+        transaction_id: transaction_id,
+        sampled: sampled |> decode_sampled(),
+        priority: priority |> String.to_float(),
+        timestamp: timestamp |> String.to_integer()
+      }
+    }
+  end
+
+  def decode(:other, vendor, value) do
+    %{vendor: vendor, value: value}
+  end
+
+  defp vendor_type(vendor) do
+    (String.contains?(vendor, "@nr") && :new_relic) || :other
+  end
+
+  defp decode_type("0"), do: "App"
+  defp decode_type("1"), do: "Browser"
+  defp decode_type("2"), do: "Mobile"
+
+  defp encode_type("App"), do: "0"
+  defp encode_type("Browser"), do: "1"
+  defp encode_type("Mobile"), do: "2"
+
+  defp decode_sampled("1"), do: true
+  defp decode_sampled("0"), do: false
+
+  defp encode_sampled(true), do: "1"
+  defp encode_sampled(false), do: "0"
 end
--- test/wc3_trace_context_test.exs
+++ test/wc3_trace_context_test.exs
@@ -2,25 +2,32 @@ defmodule W3CTraceContextTest do
   use ExUnit.Case
 
   alias NewRelic.W3CTraceContext.TraceParent
+  alias NewRelic.W3CTraceContext.TraceState
 
   test "decode traceparent header" do
-    assert_invalid("00-00000000000000000000000000000000-00000000000000ea-01")
-    assert_invalid("00-000000000000000000000000000000AA-0000000000000000-01")
-    assert_invalid("asdf")
+    assert_invalid(TraceParent, "00-00000000000000000000000000000000-00000000000000ea-01")
+    assert_invalid(TraceParent, "00-000000000000000000000000000000AA-0000000000000000-01")
+    assert_invalid(TraceParent, "asdf")
 
-    assert_valid("00-000000000000000000000000000000AA-00000000000000ea-01")
-    assert_valid("00-000000000000000000000000000000AA-00000000000000ea-00")
+    assert_valid(TraceParent, "00-000000000000000000000000000000AA-00000000000000ea-01")
+    assert_valid(TraceParent, "00-000000000000000000000000000000AA-00000000000000ea-00")
   end
 
-  def assert_valid(header) do
-    assert header |> String.downcase() ==
+  test "parse tracestate" do
+    assert_valid(
+      TraceState,
+      "190@nr=0-0-709288-8599547-f85f42fd82a4cf1d-164d3b4b0d09cb05-1-0.789-1563574856827,@vendor=value"
+    )
+  end
+
+  def assert_valid(module, header) do
+    assert String.downcase(header) ==
              header
-             |> TraceParent.decode()
-             |> IO.inspect()
-             |> TraceParent.encode()
+             |> module.decode()
+             |> module.encode()
   end
 
-  def assert_invalid(header) do
-    assert :invalid == TraceParent.decode(header)
+  def assert_invalid(module, header) do
+    assert :invalid == module.decode(header)
   end
 end
