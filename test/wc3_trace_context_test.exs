643fe3ae0f7da10e3da4cd010dbdecaf326e54f7
Vince Foley <vince@newrelic.com>
2020-01-16 23:15:16 -0800

    verify others

--- test/wc3_trace_context_test.exs
+++ test/wc3_trace_context_test.exs
@@ -29,7 +29,7 @@ defmodule W3CTraceContextTest do
     traceparent = "00-74be672b84ddc4e4b28be285632bbc0a-27ddd2d8890283b4-01"
 
     tracestate =
-      "190@nr=0-0-1349956-41346604-27ddd2d8890283b4-b28be285632bbc0a-1-1.1273-1569367663277"
+      "190@nr=0-0-1349956-41346604-27ddd2d8890283b4-b28be285632bbc0a-1-1.1273-1569367663277,@vendor=value"
 
     conn =
       Plug.Test.conn(:get, "/")
