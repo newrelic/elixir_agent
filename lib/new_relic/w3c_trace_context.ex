8c39449c872d5c5d5a7cb31b13940157df8ad3bd
Vince Foley <vince@newrelic.com>
2020-01-16 23:20:26 -0800

    rename vars

--- lib/new_relic/w3c_trace_context.ex
+++ lib/new_relic/w3c_trace_context.ex
@@ -5,34 +5,33 @@ defmodule NewRelic.W3CTraceContext do
 
   @w3c_traceparent "traceparent"
   @w3c_tracestate "tracestate"
+  @hex 16
 
   def extract(conn) do
     [traceparent_header | _] = Plug.Conn.get_req_header(conn, @w3c_traceparent)
     [tracestate_header | _] = Plug.Conn.get_req_header(conn, @w3c_tracestate)
 
     traceparent = TraceParent.decode(traceparent_header)
-    tracestate = TraceState.decode(tracestate_header)
+    %{members: members} = TraceState.decode(tracestate_header)
 
-    {[newrelic], others} =
+    {[%{value: tracestate}], others} =
       Enum.split_with(
-        tracestate.members,
-        &(&1.key == :new_relic &&
-            &1.value.trusted_account_key == AgentRun.trusted_account_key())
+        members,
+        &(&1.key == :new_relic && &1.value.trusted_account_key == AgentRun.trusted_account_key())
       )
 
-    # need to store other members for outgoing
     %Context{
       source: {:w3c, others},
-      type: newrelic.value.parent_type,
-      account_id: newrelic.value.account_id,
-      app_id: newrelic.value.app_id,
-      parent_id: newrelic.value.transaction_id,
-      span_guid: newrelic.value.span_id,
+      type: tracestate.parent_type,
+      account_id: tracestate.account_id,
+      app_id: tracestate.app_id,
+      parent_id: tracestate.transaction_id,
+      span_guid: tracestate.span_id,
       trace_id: traceparent.trace_id,
-      trust_key: newrelic.value.trusted_account_key,
-      priority: newrelic.value.priority,
-      sampled: newrelic.value.sampled,
-      timestamp: newrelic.value.timestamp
+      trust_key: tracestate.trusted_account_key,
+      priority: tracestate.priority,
+      sampled: tracestate.sampled,
+      timestamp: tracestate.timestamp
     }
   end
 
@@ -47,7 +46,7 @@ defmodule NewRelic.W3CTraceContext do
       TraceParent.encode(%TraceParent{
         version: "00",
         trace_id: context.trace_id,
-        parent_id: context.span_guid |> String.to_integer(16),
+        parent_id: context.span_guid |> String.to_integer(@hex),
         flags: %{sampled: true}
       })
 
