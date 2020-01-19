4ce2a30c6b233e5413890e013eca705c5121090b
Vince Foley <vince@newrelic.com>
2020-01-18 21:40:27 -0800

    move tests

--- test/wc3_trace_context_test.exs
+++ test/wc3_trace_context_test.exs
@@ -1,8 +1,54 @@
 defmodule W3CTraceContextTest do
   use ExUnit.Case
+  use Plug.Test
 
   alias NewRelic.W3CTraceContext.TraceParent
   alias NewRelic.W3CTraceContext.TraceState
+  alias NewRelic.Harvest.Collector
+  alias NewRelic.DistributedTrace
+
+  @w3c_traceparent "traceparent"
+  @w3c_tracestate "tracestate"
+
+  defmodule TestPlugApp do
+    use Plug.Router
+    use NewRelic.Transaction
+    use NewRelic.Tracer
+
+    plug(:match)
+    plug(:dispatch)
+
+    get "/w3c" do
+      [_, {_, traceparent}, {_, tracestate}] = NewRelic.create_distributed_trace_payload(:http)
+
+      send_resp(conn, 200, "#{traceparent}|#{tracestate}")
+    end
+
+    @trace :external_call
+    def external_call() do
+      NewRelic.create_distributed_trace_payload(:http)
+    end
+  end
+
+  setup do
+    prev_key = Collector.AgentRun.trusted_account_key()
+    Collector.AgentRun.store(:trusted_account_key, "190")
+    prev_acct = Collector.AgentRun.account_id()
+    Collector.AgentRun.store(:account_id, 190)
+
+    System.put_env("NEW_RELIC_HARVEST_ENABLED", "true")
+    System.put_env("NEW_RELIC_LICENSE_KEY", "foo")
+    send(DistributedTrace.BackoffSampler, :reset)
+
+    on_exit(fn ->
+      Collector.AgentRun.store(:trusted_account_key, prev_key)
+      Collector.AgentRun.store(:account_id, prev_acct)
+      System.delete_env("NEW_RELIC_HARVEST_ENABLED")
+      System.delete_env("NEW_RELIC_LICENSE_KEY")
+    end)
+
+    :ok
+  end
 
   test "decode traceparent header" do
     assert_invalid(TraceParent, "00-00000000000000000000000000000000-00000000000000ea-01")
@@ -35,7 +81,7 @@ defmodule W3CTraceContextTest do
     tracestate_other_vendor = ",@vendor=value"
 
     conn =
-      Plug.Test.conn(:get, "/")
+      Plug.Test.conn(:get, "/w3c")
       |> Plug.Conn.put_req_header("traceparent", traceparent)
       |> Plug.Conn.put_req_header("tracestate", tracestate)
 
@@ -60,4 +106,135 @@ defmodule W3CTraceContextTest do
   def assert_invalid(module, header) do
     assert :invalid == module.decode(header)
   end
+
+  test "Annotate Events with W3C attrs - incoming Mobile payload" do
+    TestHelper.restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.restart_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+
+    conn(:get, "/w3c")
+    |> put_req_header(@w3c_traceparent, "00-eb970877cfd349b4dcf5eb9957283bca-5f474d64b9cc9b2a-00")
+    |> put_req_header(
+      @w3c_tracestate,
+      "190@nr=0-2-332029-2827902-5f474d64b9cc9b2a-7d3efb1b173fecfa---1518469636035"
+    )
+    |> TestPlugApp.call([])
+
+    [[_, tx_attrs] | _] = TestHelper.gather_harvest(Collector.TransactionEvent.Harvester)
+
+    assert tx_attrs[:"parent.type"] == "Mobile"
+    assert tx_attrs[:"parent.account"] == "332029"
+    assert tx_attrs[:"parent.app"] == "2827902"
+    assert tx_attrs[:parentId] == "7d3efb1b173fecfa"
+    assert tx_attrs[:parentSpanId] == "5f474d64b9cc9b2a"
+    assert tx_attrs[:traceId] == "eb970877cfd349b4dcf5eb9957283bca"
+
+    [[span_attrs, _, _]] = TestHelper.gather_harvest(Collector.SpanEvent.Harvester)
+
+    assert span_attrs[:traceId] == "eb970877cfd349b4dcf5eb9957283bca"
+    assert span_attrs[:parentId] == "5f474d64b9cc9b2a"
+    # TODO:
+    # assert span_attrs[:trustedParentId] == "5f474d64b9cc9b2a"
+
+    TestHelper.pause_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.pause_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+  end
+
+  test "Annotate Events with W3C attrs - incoming agent payload" do
+    TestHelper.restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.restart_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+
+    prev_key = Collector.AgentRun.trusted_account_key()
+    Collector.AgentRun.store(:trusted_account_key, "1349956")
+
+    conn(:get, "/w3c")
+    |> put_req_header(@w3c_traceparent, "00-74be672b84ddc4e4b28be285632bbc0a-27ddd2d8890283b4-01")
+    |> put_req_header(
+      @w3c_tracestate,
+      "1349956@nr=0-0-1349956-41346604-27ddd2d8890283b4-b28be285632bbc0a-1-1.1273-1569367663277"
+    )
+    |> TestPlugApp.call([])
+
+    [[_, tx_attrs] | _] = TestHelper.gather_harvest(Collector.TransactionEvent.Harvester)
+
+    assert tx_attrs[:"parent.type"] == "App"
+    assert tx_attrs[:"parent.account"] == "1349956"
+    assert tx_attrs[:"parent.app"] == "41346604"
+    assert tx_attrs[:parentId] == "b28be285632bbc0a"
+    assert tx_attrs[:parentSpanId] == "27ddd2d8890283b4"
+    assert tx_attrs[:sampled] == true
+    assert tx_attrs[:priority] == 1.1273
+    assert tx_attrs[:traceId] == "74be672b84ddc4e4b28be285632bbc0a"
+
+    [[span_attrs, _, _]] = TestHelper.gather_harvest(Collector.SpanEvent.Harvester)
+
+    assert span_attrs[:traceId] == "74be672b84ddc4e4b28be285632bbc0a"
+    assert span_attrs[:parentId] == "27ddd2d8890283b4"
+    # TODO:
+    # assert span_attrs[:trustedParentId] == "27ddd2d8890283b4"
+
+    TestHelper.pause_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.pause_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+    Collector.AgentRun.store(:trusted_account_key, prev_key)
+  end
+
+  test "Annotate Events with W3C attrs - incoming non-NR parentId payload" do
+    TestHelper.restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.restart_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+
+    prev_key = Collector.AgentRun.trusted_account_key()
+    Collector.AgentRun.store(:trusted_account_key, "33")
+
+    conn(:get, "/w3c")
+    |> put_req_header(@w3c_traceparent, "00-87b1c9a429205b25e5b687d890d4821f-7d3efb1b173fecfa-00")
+    |> put_req_header(
+      @w3c_tracestate,
+      "dd=YzRiMTIxODk1NmVmZTE4ZQ,33@nr=0-0-33-5043-27ddd2d8890283b4-5569065a5b1313bd-1-1.23456-1518469636025"
+    )
+    |> TestPlugApp.call([])
+
+    [[_, tx_attrs] | _] = TestHelper.gather_harvest(Collector.TransactionEvent.Harvester)
+
+    assert tx_attrs[:"parent.type"] == "App"
+    assert tx_attrs[:"parent.account"] == "33"
+    assert tx_attrs[:"parent.app"] == "5043"
+    assert tx_attrs[:parentId] == "5569065a5b1313bd"
+    assert tx_attrs[:parentSpanId] == "7d3efb1b173fecfa"
+    assert tx_attrs[:sampled] == true
+    assert tx_attrs[:priority] == 1.23456
+    assert tx_attrs[:traceId] == "87b1c9a429205b25e5b687d890d4821f"
+
+    [[span_attrs, _, _]] = TestHelper.gather_harvest(Collector.SpanEvent.Harvester)
+
+    assert span_attrs[:traceId] == "87b1c9a429205b25e5b687d890d4821f"
+    assert span_attrs[:parentId] == "7d3efb1b173fecfa"
+    # TODO:
+    # assert span_attrs[:trustedParentId] == "27ddd2d8890283b4"
+    # assert span_attrs[:tracingVendors] == "dd"
+
+    TestHelper.pause_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+    TestHelper.pause_harvest_cycle(Collector.SpanEvent.HarvestCycle)
+    Collector.AgentRun.store(:trusted_account_key, prev_key)
+  end
+
+  alias NewRelic.W3CTraceContext.{TraceParent, TraceState}
+
+  # test "Generate expected outbound W3C headers" do
+  #   response =
+  #     conn(:get, "/w3c")
+  #     |> put_req_header(@dt_header, generate_inbound_payload())
+  #     |> TestPlugApp.call([])
+
+  #   [traceparent_header, tracestate_header] =
+  #     response.resp_body
+  #     |> String.split("|")
+
+  #   _traceparent = TraceParent.decode(traceparent_header)
+
+  #   assert traceparent_header =~ "d6b4ba0c3a712ca"
+
+  #   {tracestate, _} = TraceState.decode(tracestate_header) |> TraceState.newrelic()
+
+  #   assert tracestate.account_id == "190"
+  #   # ...
+  # end
 end
