e46682a7f8bb25ba494b1343606e0a6f9d5a871c
Vince Foley <vincefoley@gmail.com>
2020-02-21 09:57:26 -0800

    remove catchall

--- lib/new_relic/telemetry/ecto/metadata.ex
+++ lib/new_relic/telemetry/ecto/metadata.ex
@@ -50,10 +50,6 @@ defmodule NewRelic.Telemetry.Ecto.Metadata do
 
   def parse(%{result: {:error, _}}), do: :ignore
 
-  def parse(_) do
-    raise "Unsupported ecto adapter"
-  end
-
   def capture(regex, query, match) do
     Regex.named_captures(regex, query)[match]
   end
