1f3b24edb65af187ddb26a2d26ae801337e06e19
Vince Foley <vince@newrelic.com>
2020-03-01 22:25:56 -0800

    Handle ecto stream telemetry events

--- examples/apps/ecto_example/lib/ecto_example/count.ex
+++ examples/apps/ecto_example/lib/ecto_example/count.ex
@@ -1,6 +1,11 @@
 defmodule EctoExample.Count do
   use Ecto.Schema
 
+  def all do
+    import Ecto.Query
+    from(c in EctoExample.Count, select: c)
+  end
+
   schema "counts" do
     timestamps()
   end
