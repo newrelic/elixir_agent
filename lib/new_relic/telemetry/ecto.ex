edbe8f5d43829b1f16bb5b44a9f9722027254149
Vince Foley <vince@newrelic.com>
2020-04-05 23:41:00 -0700

    Detect Ecto Repo via repo init event

--- lib/new_relic/telemetry/ecto.ex
+++ lib/new_relic/telemetry/ecto.ex
@@ -18,22 +18,24 @@ defmodule NewRelic.Telemetry.Ecto do
   SQL query collection via configuration. See `NewRelic.Config` for details.
   """
 
-  def start_link(otp_app) do
-    enabled = NewRelic.Config.feature?(:ecto_instrumentation)
-    ecto_repos = Application.get_env(otp_app, :ecto_repos)
-    config = extract_config(otp_app, ecto_repos)
+  def start_link(repo: repo, opts: opts) do
+    config = %{
+      enabled?: NewRelic.Config.feature?(:ecto_instrumentation),
+      collect_sql?: NewRelic.Config.feature?(:sql_collection),
+      handler_id: {:new_relic_ecto, repo},
+      event: opts[:telemetry_prefix] ++ [:query],
+      opts: opts
+    }
 
-    GenServer.start_link(__MODULE__, config: config, enabled: enabled)
+    GenServer.start_link(__MODULE__, config)
   end
 
-  def init(config: _, enabled: false), do: :ignore
+  def init(%{enabled?: false}), do: :ignore
 
-  def init(config: config, enabled: true) do
-    log(config)
-
-    :telemetry.attach_many(
+  def init(%{enabled?: true} = config) do
+    :telemetry.attach(
       config.handler_id,
-      config.events,
+      config.event,
       &NewRelic.Telemetry.Ecto.Handler.handle_event/4,
       config
     )
@@ -44,60 +46,5 @@ defmodule NewRelic.Telemetry.Ecto do
 
   def terminate(_reason, %{handler_id: handler_id}) do
     :telemetry.detach(handler_id)
-  end
-
-  defp extract_config(otp_app, ecto_repos) do
-    %{
-      otp_app: otp_app,
-      events: extract_events(otp_app, ecto_repos),
-      repo_configs: extract_repo_configs(otp_app, ecto_repos),
-      collect_sql?: NewRelic.Config.feature?(:sql_collection),
-      handler_id: {:new_relic_ecto, otp_app}
-    }
-  end
-
-  defp extract_events(otp_app, ecto_repos) do
-    Enum.map(ecto_repos, fn repo ->
-      ecto_telemetry_prefix(otp_app, repo) ++ [:query]
-    end)
-  end
-
-  defp extract_repo_configs(otp_app, ecto_repos) do
-    Enum.into(ecto_repos, %{}, fn repo ->
-      {repo, extract_repo_config(otp_app, repo)}
-    end)
-  end
-
-  defp extract_repo_config(otp_app, repo) do
-    Application.get_env(otp_app, repo)
-    |> Map.new()
-    |> case do
-      %{url: url} ->
-        uri = URI.parse(url)
-
-        %{
-          hostname: uri.host,
-          port: uri.port,
-          database: uri.path |> String.trim_leading("/")
-        }
-
-      config ->
-        config
-    end
-  end
-
-  defp ecto_telemetry_prefix(otp_app, repo) do
-    Application.get_env(otp_app, repo)
-    |> Keyword.get_lazy(:telemetry_prefix, fn ->
-      repo
-      |> Module.split()
-      |> Enum.map(&(&1 |> Macro.underscore() |> String.to_atom()))
-    end)
-  end
-
-  defp log(%{repo_configs: repo_configs}) do
-    for {repo, _config} <- repo_configs do
-      NewRelic.log(:info, "Detected Ecto Repo `#{inspect(repo)}`")
-    end
   end
 end
--- lib/new_relic/telemetry/ecto/supervisor.ex
+++ lib/new_relic/telemetry/ecto/supervisor.ex
@@ -3,15 +3,29 @@ defmodule NewRelic.Telemetry.Ecto.Supervisor do
 
   use DynamicSupervisor
 
+  @ecto_repo_init [:ecto, :repo, :init]
+
   def start_link() do
     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  def start_child(otp_app) do
-    DynamicSupervisor.start_child(__MODULE__, {NewRelic.Telemetry.Ecto, otp_app})
+  def init(:ok) do
+    :telemetry.attach(
+      {:new_relic_ecto, :supervisor},
+      @ecto_repo_init,
+      &__MODULE__.handle_event/4,
+      %{}
+    )
+
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 
-  def init(:ok) do
-    DynamicSupervisor.init(strategy: :one_for_one)
+  def handle_event(@ecto_repo_init, _, %{repo: repo, opts: opts}, _) do
+    NewRelic.log(:info, "Detected Ecto Repo `#{inspect(repo)}`")
+
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {NewRelic.Telemetry.Ecto, [repo: repo, opts: opts]}
+    )
   end
 end
