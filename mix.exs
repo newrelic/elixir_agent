b548f33751e706d2960a635c7a58a3ac75f2f89d
Vince Foley <vincefoley@gmail.com>
2020-05-26 14:27:10 -0700

    Update plug_cowboy dep version

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule NewRelic.Mixfile do
       {:telemetry, "~> 0.4"},
       # Instrumentation:
       {:plug, ">= 1.10.1", optional: true},
-      {:plug_cowboy, ">= 2.2.1", optional: true},
+      {:plug_cowboy, ">= 2.2.2", optional: true},
       {:ecto_sql, ">= 3.4.0", optional: true},
       {:ecto, ">= 3.4.1", optional: true}
     ]
