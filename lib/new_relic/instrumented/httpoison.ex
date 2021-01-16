3104d758c276df8f2c8021283962ae85b7040f94
Vince Foley <vincefoley@gmail.com>
2021-01-15 22:38:24 -0800

    Fix compile warnings

--- lib/new_relic/instrumented/httpoison.ex
+++ lib/new_relic/instrumented/httpoison.ex
@@ -36,97 +36,97 @@ if Code.ensure_loaded?(HTTPoison) do
     @trace {:get, category: :external}
     def get(url, headers \\ [], options \\ []) do
       headers = instrument("GET", url, headers)
-      HTTPoison.get(url, headers, options)
+      apply(HTTPoison, :get, [url, headers, options])
     end
 
     @trace {:get!, category: :external}
     def get!(url, headers \\ [], options \\ []) do
       headers = instrument("GET", url, headers)
-      HTTPoison.get!(url, headers, options)
+      apply(HTTPoison, :get!, [url, headers, options])
     end
 
     @trace {:put, category: :external}
     def put(url, body \\ "", headers \\ [], options \\ []) do
       headers = instrument("PUT", url, headers)
-      HTTPoison.put(url, body, headers, options)
+      apply(HTTPoison, :put, [url, body, headers, options])
     end
 
     @trace {:put!, category: :external}
     def put!(url, body \\ "", headers \\ [], options \\ []) do
       headers = instrument("PUT", url, headers)
-      HTTPoison.put!(url, body, headers, options)
+      apply(HTTPoison, :put!, [url, body, headers, options])
     end
 
     @trace {:head, category: :external}
     def head(url, headers \\ [], options \\ []) do
       headers = instrument("HEAD", url, headers)
-      HTTPoison.head(url, headers, options)
+      apply(HTTPoison, :head, [url, headers, options])
     end
 
     @trace {:head!, category: :external}
     def head!(url, headers \\ [], options \\ []) do
       headers = instrument("HEAD", url, headers)
-      HTTPoison.head!(url, headers, options)
+      apply(HTTPoison, :head!, [url, headers, options])
     end
 
     @trace {:post, category: :external}
     def post(url, body, headers \\ [], options \\ []) do
       headers = instrument("POST", url, headers)
-      HTTPoison.post(url, body, headers, options)
+      apply(HTTPoison, :post, [url, body, headers, options])
     end
 
     @trace {:post!, category: :external}
     def post!(url, body, headers \\ [], options \\ []) do
       headers = instrument("POST", url, headers)
-      HTTPoison.post!(url, body, headers, options)
+      apply(HTTPoison, :post!, [url, body, headers, options])
     end
 
     @trace {:patch, category: :external}
     def patch(url, body, headers \\ [], options \\ []) do
       headers = instrument("PATCH", url, headers)
-      HTTPoison.patch(url, body, headers, options)
+      apply(HTTPoison, :patch, [url, body, headers, options])
     end
 
     @trace {:patch!, category: :external}
     def patch!(url, body, headers \\ [], options \\ []) do
       headers = instrument("PATCH", url, headers)
-      HTTPoison.patch!(url, body, headers, options)
+      apply(HTTPoison, :patch!, [url, body, headers, options])
     end
 
     @trace {:delete, category: :external}
     def delete(url, headers \\ [], options \\ []) do
       headers = instrument("DELETE", url, headers)
-      HTTPoison.delete(url, headers, options)
+      apply(HTTPoison, :delete, [url, headers, options])
     end
 
     @trace {:delete!, category: :external}
     def delete!(url, headers \\ [], options \\ []) do
       headers = instrument("DELETE", url, headers)
-      HTTPoison.delete!(url, headers, options)
+      apply(HTTPoison, :delete!, [url, headers, options])
     end
 
     @trace {:options, category: :external}
     def options(url, headers \\ [], options \\ []) do
       headers = instrument("OPTIONS", url, headers)
-      HTTPoison.options(url, headers, options)
+      apply(HTTPoison, :options, [url, headers, options])
     end
 
     @trace {:options!, category: :external}
     def options!(url, headers \\ [], options \\ []) do
       headers = instrument("OPTIONS", url, headers)
-      HTTPoison.options!(url, headers, options)
+      apply(HTTPoison, :options!, [url, headers, options])
     end
 
     @trace {:request, category: :external}
     def request(method, url, body \\ "", headers \\ [], options \\ []) do
       headers = instrument(String.upcase(to_string(method)), url, headers)
-      HTTPoison.request(method, url, body, headers, options)
+      apply(HTTPoison, :request, [method, url, body, headers, options])
     end
 
     @trace {:request!, category: :external}
     def request!(method, url, body \\ "", headers \\ [], options \\ []) do
       headers = instrument(String.upcase(to_string(method)), url, headers)
-      HTTPoison.request!(method, url, body, headers, options)
+      apply(HTTPoison, :request!, [method, url, body, headers, options])
     end
   end
 end
