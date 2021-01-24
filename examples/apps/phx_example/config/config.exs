ff4d9ddb0a2560499f5ec14ade8c622da5fac6a2
Vince Foley <vince@newrelic.com>
2021-01-24 15:59:19 -0800

    fix port for phx example

--- examples/apps/phx_example/config/config.exs
+++ examples/apps/phx_example/config/config.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :phx_example, PhxExampleWeb.Endpoint,
   url: [host: "localhost"],
   render_errors: [view: PhxExampleWeb.ErrorView, accepts: ~w(html json), layout: false],
-  http: [port: 4003],
+  http: [port: 4004],
   server: true
 
 config :phoenix, :json_library, Jason
