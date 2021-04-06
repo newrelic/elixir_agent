05f685057201840cbebd550359f55057ef9afc1b
Vince Foley <vincefoley@gmail.com>
2021-04-06 10:21:32 -0700

    Fix parsing of other SELECTs

--- lib/new_relic/telemetry/ecto/metadata.ex
+++ lib/new_relic/telemetry/ecto/metadata.ex
@@ -2,30 +2,19 @@ defmodule NewRelic.Telemetry.Ecto.Metadata do
   @moduledoc false
 
   def parse(%{result: {:ok, %{__struct__: Postgrex.Cursor}}}), do: :ignore
+  def parse(%{result: {:ok, %{__struct__: MyXQL.Cursor}}}), do: :ignore
 
-  def parse(%{
-        query: query,
-        result: {_ok_or_error, %{__struct__: struct}}
-      })
+  def parse(%{query: query, result: {_ok_err, %{__struct__: struct}}})
       when struct in [Postgrex.Result, Postgrex.Error] do
     {"Postgres", parse_query(query)}
   end
 
-  def parse(%{result: {:ok, %{__struct__: MyXQL.Cursor}}}), do: :ignore
-
-  def parse(%{
-        query: query,
-        result: {_ok_or_error, %{__struct__: struct}}
-      })
+  def parse(%{query: query, result: {_ok_err, %{__struct__: struct}}})
       when struct in [MyXQL.Result, MyXQL.Error] do
     {"MySQL", parse_query(query)}
   end
 
-  def parse(%{
-        query: query,
-        repo: repo,
-        result: {_ok_or_error, %{__struct__: _result_struct}}
-      }) do
+  def parse(%{query: query, repo: repo, result: {_ok_err, %{__struct__: _struct}}}) do
     [adaapter | _] = repo.__adapter__() |> Module.split() |> Enum.reverse()
     {adaapter, parse_query(query)}
   end
@@ -33,21 +22,18 @@ defmodule NewRelic.Telemetry.Ecto.Metadata do
   def parse(%{result: {:ok, _}}), do: :ignore
   def parse(%{result: {:error, _}}), do: :ignore
 
-  defp parse_query(query) do
-    {operation, table} =
-      case query do
-        "SELECT" <> _ -> {"select", table_name(:select, query)}
-        "INSERT" <> _ -> {"insert", table_name(:insert, query)}
-        "UPDATE" <> _ -> {"update", table_name(:update, query)}
-        "DELETE" <> _ -> {"delete", table_name(:delete, query)}
-        "CREATE TABLE" <> _ -> {"create", table_name(:create, query)}
-        "begin" -> {"begin", "other"}
-        "commit" -> {"commit", "other"}
-        "rollback" -> {"rollback", "other"}
-        _ -> {"other", "other"}
-      end
-
-    {table, operation}
+  def parse_query(query) do
+    case query do
+      "SELECT" <> _ -> parse_query(:select, query)
+      "INSERT" <> _ -> parse_query(:insert, query)
+      "UPDATE" <> _ -> parse_query(:update, query)
+      "DELETE" <> _ -> parse_query(:delete, query)
+      "CREATE TABLE" <> _ -> parse_query(:create, query)
+      "begin" -> {:begin, :other}
+      "commit" -> {:commit, :other}
+      "rollback" -> {:rollback, :other}
+      _ -> {:other, :other}
+    end
   end
 
   # Table name escaping
@@ -64,8 +50,10 @@ defmodule NewRelic.Telemetry.Ecto.Metadata do
     delete: ~r/FROM (?<table>\S+)/,
     create: ~r/CREATE TABLE( IF NOT EXISTS)? (?<table>\S+)/
   }
-  defp table_name(query_type, query) do
-    Regex.named_captures(@capture[query_type], query)["table"]
-    |> String.replace(@esc, "")
+  def parse_query(operation, query) do
+    case Regex.named_captures(@capture[operation], query) do
+      %{"table" => table} -> {operation, String.replace(table, @esc, "")}
+      _ -> {:other, :other}
+    end
   end
 end
