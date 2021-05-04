81d55fd433a5124c39e8ee54eabbf9aa67a37e9e
Andrew Rosa <andrewhr@me.com>
2021-05-04 11:46:30 -0300

    Add HTTPoison as optional dependency
    We have a few libraries wrapping HTTPoison that optionally
    instrument if NewRelic is present during compilation. Due the lack of
    this optional dep here, the compilation process is not deterministic and
    may end up with cases where both libraries are included (our wrapper and
    NewRelic) and still have no instrumentation.

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule NewRelic.Mixfile do
       {:phoenix, ">= 1.5.5", optional: true},
       {:ecto_sql, ">= 3.4.0", optional: true},
       {:ecto, ">= 3.4.1", optional: true},
+      {:httpoison, ">= 1.0.0", optional: true},
       {:redix, ">= 0.11.0", optional: true}
     ]
   end
