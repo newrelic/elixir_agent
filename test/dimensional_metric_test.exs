35d99b87858f0b8e6326a95469213b4057059163
xi xia <xxia@newrelic.com>
2023-09-14 17:23:34 -0700

    send the metrics, not the useless key

--- test/dimensional_metric_test.exs
+++ test/dimensional_metric_test.exs
@@ -9,19 +9,20 @@ defmodule DimensionalMetricTest do
     NewRelic.report_dimensional_metric(:count, "memory.foo_baz", 100, %{cpu: 1000})
     NewRelic.report_dimensional_metric(:summary, "memory.foo_bar", 50, %{cpu: 2000})
 
-    [%{common: common, metrics: metrics}] =
+    [%{common: common, metrics: metrics_map}] =
       TestHelper.gather_harvest(NewRelic.Harvest.TelemetrySdk.DimensionalMetrics.Harvester)
 
+    metrics = Map.values(metrics_map)
     assert common["interval.ms"] > 0
     assert common["timestamp"] > 0
 
     assert length(metrics) == 2
     [metric1, metric2] = metrics
-    assert metric1.name == "memory.foo_bar"
-    assert metric1.type == :summary
+    assert metric1.name == "memory.foo_baz"
+    assert metric1.type == :count
 
-    assert metric2.name == "memory.foo_baz"
-    assert metric2.type == :count
+    assert metric2.name == "memory.foo_bar"
+    assert metric2.type == :summary
 
     TestHelper.pause_harvest_cycle(NewRelic.Harvest.TelemetrySdk.DimensionalMetrics.HarvestCycle)
   end
