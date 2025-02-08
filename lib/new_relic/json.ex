aff7ddd67e3cd04c8a08607aeffaeccbb373d043
Tyler Witt <Ty.Mo.Witt@gmail.com>
2025-02-08 21:35:03 +0900

    Update lib/new_relic/json.ex
    Co-authored-by: Vince Foley <39946+binaryseed@users.noreply.github.com>

--- lib/new_relic/json.ex
+++ lib/new_relic/json.ex
@@ -1,15 +1,19 @@
 defmodule NewRelic.JSON do
   @moduledoc false
 
-  if Code.ensure_loaded?(JSON) do
-    defdelegate decode(data), to: JSON
-    defdelegate decode!(data), to: JSON
-    defdelegate encode!(data), to: JSON
+  cond do
+    Code.ensure_loaded?(JSON) ->
+      def decode(data), do: apply(JSON, :decode, [data])
+      def decode!(data), do: apply(JSON, :decode!, [data])
+      def encode!(data), do: apply(JSON, :encode!, [data])
+
+    Code.ensure_loaded?(Jason) ->
+      def decode(data), do: apply(Jason, :decode, [data])
+      def decode!(data), do: apply(Jason, :decode!, [data])
+      def encode!(data), do: apply(Jason, :encode!, [data])
+
+    true ->
+      raise "[:new_relic_agent] No JSON library found, please add :jason as a dependency"
   end
 
-  if Code.ensure_loaded?(Jason) do
-    defdelegate decode(data), to: Jason
-    defdelegate decode!(data), to: Jason
-    defdelegate encode!(data), to: Jason
-  end
 end
