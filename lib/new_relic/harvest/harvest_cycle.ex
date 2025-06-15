b0d811bdd4b1b5643214881fa74ef821d64fd307
Stefano Gessa <stefano@gessa.net>
2025-06-15 18:17:24 +0700

    Handle errors in `Harvest.HarvestCycle.send_harvest/2`

--- lib/new_relic/harvest/harvest_cycle.ex
+++ lib/new_relic/harvest/harvest_cycle.ex
@@ -131,11 +131,20 @@ defmodule NewRelic.Harvest.HarvestCycle do
           :exit, _exit ->
             NewRelic.log(:error, "Failed to send harvest from #{inspect(supervisor)}")
         after
-          DynamicSupervisor.terminate_child(supervisor, harvester)
+          terminate_child(supervisor, harvester)
         end
       end,
       shutdown: @harvest_timeout
     )
+  end
+
+  defp terminate_child(supervisor, harvester) do
+    try do
+      DynamicSupervisor.terminate_child(supervisor, harvester)
+    catch
+      :exit, _exit ->
+        :ok
+    end
   end
 
   defp stop_harvest_cycle(timer), do: timer && Process.cancel_timer(timer)
